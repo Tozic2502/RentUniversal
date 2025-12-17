c7035af8ca81648b36d294fd470706df51575a08
Mikkel <Mikcollatz@gmail.com>
2025-12-17 11:54:12 +0100

    again

--- RentUniversal.Domain/Entities/Item.cs
+++ RentUniversal.Domain/Entities/Item.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,8 @@ public class Item
     /// <summary>
     /// Gets or sets the unique identifier for the item.
     /// </summary>
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
     /// <summary>
