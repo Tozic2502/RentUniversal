dd4768c2df64e74863390800105ba2db16f08fb6
Mikkel <Mikcollatz@gmail.com>
2025-12-17 11:53:01 +0100

    auto number

--- RentUniversal.Domain/Entities/Item.cs
+++ RentUniversal.Domain/Entities/Item.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ public class Item
     /// <summary>
     /// Gets or sets the unique identifier for the item.
     /// </summary>
-    public string Id { get; set; } = "";
+    public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
 
     /// <summary>
     /// Gets or sets the name of the item.
