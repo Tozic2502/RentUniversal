6f80db4f66d3ee46ca16826595cdbfa615bee3f5
preaz <broderboysen@protonmail.com>
2025-12-17 13:07:23 +0100

    Updated a bunch of stuff

--- rentuniversal-client/vite.config.js
+++ rentuniversal-client/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    server: {
+        proxy: {
+            '/test': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            }
+        }
+    },
     plugins: [react()],
     build: {
         outDir: "dist",
